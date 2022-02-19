76d020f59e8110bb0261c5971fc87df238656d4f
mickey243 <mithleshk522@gmail.com>
2022-02-19 20:36:37 +0530

    adding navigation to cart button

--- src/components/AllProducts/CartPageView.js
+++ src/components/AllProducts/CartPageView.js
@@ -1,38 +1,55 @@
 import React from "react";
 import "./CartPageView.css";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardSubtitle,
-  CardText,
-  CardTitle,
-  Col,
-  Row,
-} from "reactstrap";
+import { Card, Col, Row, Input } from "reactstrap";
+import BaseUi from "../BaseUi/BaseUi";
 
 const CartPageView = () => {
   return (
-    <Row className="text-center">
-      <Col xs={12} sm={12} md={6}>
-        <div className="cartpage__products">
-          <Card>
-            <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-      </Col>
-      <Col xs={12} sm={12} md={6}>
-        <div className="cartpage__checkout">checkoutdetails</div>
-      </Col>
-    </Row>
+    <BaseUi>
+      <Row className="text-center">
+        <h1>Check Out Page</h1>
+        <Col xs={12} sm={12} md={6}>
+          <div className="cartpage__products">
+            <Card className="cartpage__product_card">
+              <div className="cartpage__products__img">
+                <img
+                  src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                  alt="product"
+                />
+              </div>
+              <div className="cartpage__products__info">
+                <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                <p>Quantity : 10</p>
+                <p>Total : 1000</p>
+                <div className="cartpage__products__info_btn">
+                  <button className="cart__product_btn_add mx-2">Add</button>
+                  <button className="cart__product_btn_remove">Remove</button>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </Col>
+        <Col xs={12} sm={12} md={6}>
+          <div className="cartpage__checkout">
+            {/* <Card> */}
+            <p>Total Amount: 1000 </p>
+            <p>Total Count: 15 </p>
+
+            <Input type="select" className="mx-5">
+              <option>Please Select Payment Method</option>
+              <option>C.O.D</option>
+              <option>PhonePay</option>
+              <option>GooglePay</option>
+            </Input>
+
+            <button className="mx-5 cartpage__checkout__btn">Check Out</button>
+            {/* </Card> */}
+
+            <div className="cartpage__checkout__payment"></div>
+          </div>
+        </Col>
+      </Row>
+    </BaseUi>
   );
 };
 
--- src/components/BaseUi/BaseUi.js
+++ src/components/BaseUi/BaseUi.js
@@ -2,12 +2,20 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./BaseUi.css";
 import { productCategorySliceAction } from "../../redux/ProductCategoty";
+import { NavLink } from "react-router-dom";
 const BaseUi = (props) => {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const productTotalCount = useSelector(
     (state) => state.productStore.totalProductCount
   );
+  // <NavLink
+  //           // target="_blank"
+  //           className="btn btn-warning"
+  //           to={`/product/${id}`}
+  //         >
+  //           View Product
+  //         </NavLink>
 
   return (
     <>
@@ -20,9 +28,14 @@ const BaseUi = (props) => {
         <div className="baseui__navbar">
           <ul>
             <li>
-              <button className="baseui__btn cart__btn" style={{ width: 200 }}>
-                {productTotalCount} cart
-              </button>
+              <NavLink
+                className="baseui__btn cart__btn text-center"
+                style={{ width: 200 }}
+                to={`/cart`}
+              >
+                <span className="cart__btn__txt">{productTotalCount}+</span>
+                <span> cart</span>
+              </NavLink>
             </li>
             <li>
               <button className="baseui__btn">About</button>
